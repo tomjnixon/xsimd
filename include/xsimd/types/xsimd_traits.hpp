195706789edb17de1a281ad6f86aea4faf182982
Thomas Nixon <tom@tomn.co.uk>
2021-11-11 22:54:15 +0000

    fix simd_condition formatting

--- include/xsimd/types/xsimd_traits.hpp
+++ include/xsimd/types/xsimd_traits.hpp
@@ -113,7 +113,20 @@ namespace xsimd
         template <class T1, class T2>
         struct simd_condition
         {
-            static constexpr bool value = (std::is_same<T1, T2>::value && !std::is_same<T1, bool>::value) || (std::is_same<T1, bool>::value && !std::is_same<T2, bool>::value) || std::is_same<T1, float>::value || std::is_same<T1, double>::value || std::is_same<T1, int8_t>::value || std::is_same<T1, uint8_t>::value || std::is_same<T1, int16_t>::value || std::is_same<T1, uint16_t>::value || std::is_same<T1, int32_t>::value || std::is_same<T1, uint32_t>::value || std::is_same<T1, int64_t>::value || std::is_same<T1, uint64_t>::value || std::is_same<T1, char>::value || detail::is_complex<T1>::value;
+            static constexpr bool value = ((std::is_same<T1, T2>::value && !std::is_same<T1, bool>::value)
+                                           || (std::is_same<T1, bool>::value && !std::is_same<T2, bool>::value)
+                                           || std::is_same<T1, float>::value
+                                           || std::is_same<T1, double>::value
+                                           || std::is_same<T1, int8_t>::value
+                                           || std::is_same<T1, uint8_t>::value
+                                           || std::is_same<T1, int16_t>::value
+                                           || std::is_same<T1, uint16_t>::value
+                                           || std::is_same<T1, int32_t>::value
+                                           || std::is_same<T1, uint32_t>::value
+                                           || std::is_same<T1, int64_t>::value
+                                           || std::is_same<T1, uint64_t>::value
+                                           || std::is_same<T1, char>::value
+                                           || detail::is_complex<T1>::value);
         };
 
         template <class T1, class T2, class A>
